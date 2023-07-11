6e8990f40a3131813c78594da770604b5f41d26c
dori-dev <mr.dori.dev@gmail.com>
2023-07-11 16:08:37 +0330

    Add a card element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-orange-600 text-3xl">Hello Tailwind!</h1>
+    <div className="m-4">
+      <div className="card">
+        <img
+          src="https://source.unsplash.com/random/"
+          alt=""
+          className="w-full"
+        />
+        <div className="px-6 py-4">
+          <span className="card-title">An Example Image</span>
+          <ul className="mt-2">
+            <li>
+              <strong>Views:</strong> 11K
+            </li>
+            <li>
+              <strong>Downloads:</strong> 3.3K
+            </li>
+            <li>
+              <strong>Likes:</strong> 200
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 pb-2">
+          <span className="tag">#wow</span>
+          <span className="tag">#beautiful</span>
+          <span className="tag">#tag2</span>
+          <span className="tag">#tag3</span>
+          <span className="tag">#image</span>
+          <span className="tag">#background</span>
+          <span className="tag">#tag4</span>
+          <span className="tag">#tag5</span>
+        </div>
+      </div>
     </div>
   );
 }
