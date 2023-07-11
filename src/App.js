63dd0e28ee16797119cbdd453e5df971292c98a3
dori-dev <mr.dori.dev@gmail.com>
2023-07-11 16:46:57 +0330

    Use API for get images

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
+import react, { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [term, setTerm] = useState("");
+  const getData = async () => {
+    const key = process.env.REACT_APP_PIXABAY_API_KEY;
+    const per_page = 48;
+    const { data } = await axios.get(
+      `https://pixabay.com/api/?key=${key}&per_page=${per_page}&q=${term}&image_type=photo`
+    );
+    return data.hits;
+  };
+  useEffect(() => {
+    try {
+      const data = getData();
+      data.then((result) => {
+        setImages(result);
+      });
+    } catch (error) {
+      setError(error.message);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div className="m-4">
-      <div className="card">
-        <img
-          src="https://source.unsplash.com/random/"
-          alt=""
-          className="w-full"
-        />
-        <div className="px-6 py-4">
-          <span className="card-title">An Example Image</span>
-          <ul className="mt-2">
-            <li>
-              <strong>Views:</strong> 11K
-            </li>
-            <li>
-              <strong>Downloads:</strong> 3.3K
-            </li>
-            <li>
-              <strong>Likes:</strong> 200
-            </li>
-          </ul>
+    <div className="m-4 flex flex-wrap gap-10">
+      {images.map((image) => (
+        <div className="card">
+          <img src={image.webformatURL} alt="" className="w-full" />
+          <div className="px-6 py-4">
+            <span className="card-title">Beautiful Photo By {image.user}</span>
+            <ul className="mt-2">
+              <li>
+                <strong>Views:</strong> {image.views}
+              </li>
+              <li>
+                <strong>Downloads:</strong> {image.downloads}
+              </li>
+              <li>
+                <strong>Likes:</strong> {image.likes}
+              </li>
+            </ul>
+          </div>
+          <div className="px-6 pb-2">
+            {image.tags.split(",").map((tag) => (
+              <span className="tag">#{tag}</span>
+            ))}
+          </div>
         </div>
-        <div className="px-6 pb-2">
-          <span className="tag">#wow</span>
-          <span className="tag">#beautiful</span>
-          <span className="tag">#tag2</span>
-          <span className="tag">#tag3</span>
-          <span className="tag">#image</span>
-          <span className="tag">#background</span>
-          <span className="tag">#tag4</span>
-          <span className="tag">#tag5</span>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
